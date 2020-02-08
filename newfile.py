3081313433ae1e096c032fc1ffd96a84648f16d3
RedSun-ops <55594856+RedSun-ops@users.noreply.github.com>
2020-02-08 18:58:01 +0000

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import platform
+import sys
+root = Tk()      
+canvas = Canvas(root, width = 500, height = 400)
+canvas.pack() 
+system = str(sys.platform)
+uname = platform.uname()
+if system == "linux":
+	img2 = PhotoImage(file="123.png")
+if system == "win32":
+	img2 = PhotoImage(file="windows.png")
+if system == "darwin":
+	img2 = PhotoImage(file="macosx.png")
+if system == "linux":
+	img = PhotoImage(file="linux.png")
+if system == "win32":
+	img = PhotoImage(file="win.png")
+if system == "darwin":
+	img = PhotoImage(file="macos.jpg")
+w = Label(root, text=str(uname.system) + " " + str(uname.release) + " " + str(uname.machine))
+w.pack()
+canvas.create_image(0,0, anchor=NW, image=img2)
+canvas.create_image(0,0, anchor=NW, image=img)  
+mainloop()   
---
+++ readme.txt
@@ -0,0 +1,7 @@
+this app passport :
+supported by windows xp and higher, mac os, linux
+not working with os/2
+warning! do not remove pictures!
+created by paul
+programming language : python
+modules : tkinter, platform, os, sys
